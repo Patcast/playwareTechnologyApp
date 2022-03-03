fa7590fd7641215dbe2e90cca5ad5091d4aa23d1
patocastillo <patricio.castillo2009@gmail.com>
2022-03-03 14:39:29 +0100

    hello world

--- app/src/main/java/pat/international/playwaretwo/HomeActivity.java
+++ app/src/main/java/pat/international/playwaretwo/HomeActivity.java
@@ -1,7 +1,11 @@
 package pat.international.playwaretwo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
@@ -10,6 +14,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable = new ColorDrawable(ContextCompat.getColor(this, R.color.toolBar));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
 
     }
 }
