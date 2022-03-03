66769796d4b90ca8acc20dc107b6c2700418c48d
patocastillo <patricio.castillo2009@gmail.com>
2022-03-03 15:01:51 +0100

    start Game 4

--- app/src/main/java/pat/international/playwaretwo/HomeActivity.java
+++ app/src/main/java/pat/international/playwaretwo/HomeActivity.java
@@ -15,16 +15,10 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        // Define ActionBar object
         ActionBar actionBar;
         actionBar = getSupportActionBar();
-
-        // Define ColorDrawable object and parse color
-        // using parseColor method
-        // with color hash code as its parameter
         ColorDrawable colorDrawable = new ColorDrawable(ContextCompat.getColor(this, R.color.toolBar));
 
-        // Set BackgroundDrawable
         actionBar.setBackgroundDrawable(colorDrawable);
 
     }
