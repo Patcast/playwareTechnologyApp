b2124864dbca8f59f56ee2e49ac0532dd3f879fd
Floor <FloorGeest@live.nl>
2022-03-31 15:56:31 +0200

    :(

--- app/src/main/java/pat/international/playwaretwo/ass8/ListMain.java
+++ app/src/main/java/pat/international/playwaretwo/ass8/ListMain.java
@@ -1,61 +1,27 @@
 package pat.international.playwaretwo.ass8;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import pat.international.playwaretwo.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListMain#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ListMain extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListMain() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListMain.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListMain newInstance(String param1, String param2) {
-        ListMain fragment = new ListMain();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,4 +29,40 @@ public class ListMain extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list_main, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayList<Games> games = new ArrayList<Games>();
+
+        games.add(new Games("Normal mode"));
+        games.add(new Games("Hard mode"));
+        games.add(new Games("Normal time mode practice"));
+        games.add(new Games("Hard time mode practice"));
+        games.add(new Games("View challenges"));
+
+
+       /* GamesAdapter gamesAdapter = new GamesAdapter(this, games);
+
+        ListView listView = (ListView) findViewById(R.id.listview_games);
+        listView.setAdapter(gamesAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 4:
+                        Intent viewchallenges = new Intent(MainActivity.this,ViewChallenges.class);
+                        startActivity(viewchallenges);
+                        break;
+                    case 0:
+                        Intent test = new Intent(MainActivity.this, com.example.list.Test.class);
+                        startActivity(test);
+                        break;
+                }
+            }
+        });*/
+    }
 }
+
