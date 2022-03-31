24442c9de879a6b132abfa73af9e8e162d858060
Floor <FloorGeest@live.nl>
2022-03-31 15:10:01 +0200

    :)

---
+++ app/src/main/java/pat/international/playwaretwo/ass8/Test.java
@@ -0,0 +1,17 @@
+package pat.international.playwaretwo.ass8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+
+
+    }
+}
